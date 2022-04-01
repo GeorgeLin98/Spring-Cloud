c14be1f8c88781b4ecc164b9b2b88927586e6af4
linzhuangze <linzhuangze@do1.com.cn>
2022-04-01 14:57:37 +0800

    feat：open-fegin

--- cloud-provider-payment8001/src/main/java/com/george/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/george/springcloud/controller/PaymentController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
@@ -65,6 +66,12 @@ public class PaymentController {
 
     @GetMapping(value = "/payment/lb")
     public String getPaymentLB(){
+        return serverPort;
+    }
+
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout(){
+        try { TimeUnit.SECONDS.sleep(3); }catch (Exception e) {e.printStackTrace();}
         return serverPort;
     }
 
